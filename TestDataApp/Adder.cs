a0b5bac4733bd496151f53c70c4971eef1442214
Gogs <gogs@fake.local>
2019-02-26 11:10:27 +0100

    Initial

---
+++ TestDataApp/Adder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDataApp
+{
+    public class Adder
+    {
+        public int Add(int a, int b)
+        {
+            int r = a + b;
+            return r;
+        }
+    }
+}
---
+++ TestDataApp/DisplayNameGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDataApp
+{
+    public class DisplayNameGenerator
+    {
+        private readonly PersonData personData;
+
+        public DisplayNameGenerator(PersonData personData)
+        {
+            this.personData = personData ?? throw new ArgumentNullException(nameof(personData));
+        }
+
+        public string GetDisplayName()
+        {
+            string r = String.Format("{0} {1} ({2})", personData.FirstName, personData.LastName, personData.Age);
+            return r;
+        }
+    }
+}
